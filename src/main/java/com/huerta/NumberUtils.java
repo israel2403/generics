d9e34c09ab5455ed1bcb22a84e884700cafd35d1
Israel Huerta Fer <israelhf24@gmail.com>
2025-11-03 13:24:08 -0600

    addtion generic method

--- src/main/java/com/huerta/NumberUtils.java
+++ src/main/java/com/huerta/NumberUtils.java
@@ -10,4 +10,11 @@ public class NumberUtils {
   public static <T extends Comparable<T>> T min(T item1, T item2) {
     return (item1.compareTo(item2) < 0) ? item1 : item2;
   }
+
+  public static <T extends Number> double add(T num1, T num2) {
+    if (num1 == null || num2 == null) {
+      throw new IllegalArgumentException("Numbers must not be null");
+    }
+    return num1.doubleValue() + num2.doubleValue();
+  }
 }
